0ceb3bf0ac5724814561be994c5b73548f79c280
lupitaesp <lupitaesp.com>
2022-07-24 16:48:49 -0500

    Formulario, falta estructurarlo

--- proyectoinventario/forms.py
+++ proyectoinventario/forms.py
@@ -1,11 +1,43 @@
 from django import forms
 
 from proyectoinventario.models import Assets
+from crispy_forms.helper import FormHelper
 
 class FormAssets(forms.ModelForm):
+    modelo = forms.CharField(
+         required=True,
+     )
+    serie = forms.CharField(
+         required=True,
+     )
+    marca = forms.CharField(
+         required=True,
+     )
+    cantidad = forms.IntegerField(
+         required=True,
+     )
+    tipo = forms.CharField(
+         required=True,
+     )
+    categoria = forms.TypedChoiceField(
+         required=True,
+         choices = (('stock', "Stock"), ('dañado', "Dañado")),
+        
+     )
+    estado = forms.TypedChoiceField(
+         choices = (('nuevo', "Nuevo"),('garantia', "Garantia"),('funcional', "Funcional"), ('baja', "Baja")),
+         required=True,
+      
+     )
+    accion = forms.TypedChoiceField(
+         required=True,
+         choices = ((1, "Entrada"), (0, "Salida")),
+         initial = '1',
+         widget = forms.RadioSelect,
+     )
     class Meta:
         model= Assets
-        fields = '__all__'
+        fields = ['modelo','serie','marca','cantidad','tipo','categoria','estado','accion']
         wigets = {'fecha':forms.DateInput(attrs={'type':'date'})}
 
 
